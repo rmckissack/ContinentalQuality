3969702ff00214a09ee27179edde63b08b61053d
rmckissack <randy@mckissack.net>
2019-04-24 18:48:16 -0400

    Added a button to main window so we could get to the Tally Sheet

--- src/com/continentalquality/MainWindowController.java
+++ src/com/continentalquality/MainWindowController.java
@@ -11,22 +11,37 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.Modality;
 
 import java.io.IOException;
 
 public class MainWindowController {
 
     @FXML
-    private BorderPane rootPane;
-
+    public Button tallyButton;
 
     @FXML
-    private void loadTally(ActionEvent event) throws IOException {
-        BorderPane pane = FXMLLoader.load(getClass().getResource("TallySheetWindow.fxml"));
-        rootPane.getChildren().setAll(pane);
+    public Button tallyMenuLink;
+
+    @FXML
+    public void tallyButtonClick(ActionEvent actionEvent) throws IOException {
+        Window mainWindow = tallyButton.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("TallySheetWindow.fxml"));
+        mainWindow.getScene().setRoot(newRoot);
+    }
+
+    @FXML
+    public void tallyMenuClicked(ActionEvent actionEvent) throws IOException {
+        Window mainWindow = tallyMenuLink.getScene().getWindow();
+        Parent newRoot = FXMLLoader.load(getClass().getResource("TallySheetWindow.fxml"));
+        mainWindow.getScene().setRoot(newRoot);
     }
 
 }
